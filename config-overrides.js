ae743e02dd5ff2d949c71fae291f23924d41b9a7
Ally Parker <ally.parker@red-gate.com>
2020-05-14 13:18:15 +0100

    Start overriding

--- config-overrides.js
+++ config-overrides.js
@@ -9,8 +9,37 @@ module.exports = override(
     lessOptions: {
       javascriptEnabled: true,
       modifyVars: {
-        '@primary-color': '#25b864',
-        '@border-radius-base': '6px',
+        '@red-base': '#cc0000',
+        '@blue-base': '#3c85df',
+        '@green-base': '#1aac1e',
+        '@orange-base':'#fc9003',
+        '@yellow-base': '#fed100',
+        
+        '@font-family' : '"Roboto", "Helvetica Neue", Arial, sans-serif',
+        
+        '@text-color': '#373737',
+        '@font-size-base': '16px',
+        '@link-color': '#336dc2',
+        // '@line-height-base': '26px',
+        '@heading-color': '#373737',
+        '@border-radius-base': '5px',
+
+        // Forms
+        '@input-border-color': '#6e6e6e',
+
+        // Alerts
+        '@alert-no-icon-padding-vertical': '15px',
+
+        '@alert-success-bg-color': '#f2f2f2',
+        '@alert-info-bg-color': '#f2f2f2',
+        '@alert-warning-bg-color': '#f2f2f2',
+        '@alert-error-bg-color': '#f2f2f2',
+
+        '@alert-success-border-color': '#1aac1e',
+        '@alert-info-border-color': '#3c85df',
+        '@alert-warning-border-color': '#fed100',
+        '@alert-error-border-color': '#cc0000',
+
       },
     },
   })
--- src/App.js
+++ src/App.js
@@ -1,55 +1,137 @@
-import React from 'react';
-import { Form, Select, InputNumber, DatePicker, Switch, Slider, Button, Typography } from 'antd';
-import './App.css';
+import React from "react";
+import {
+  Form,
+  Select,
+  InputNumber,
+  DatePicker,
+  Switch,
+  Slider,
+  Button,
+  Typography,
+  Checkbox,
+  Alert,
+  Layout,
+  Space,Radio
+} from "antd";
+import "./App.css";
+const { Header, Footer, Content } = Layout;
 
 const { Option } = Select;
 const { Title } = Typography;
 
 const App = () => (
   <>
-    <section style={{ textAlign: 'center', marginTop: 48, marginBottom: 60 }}>
-      <Title level={2}>
-        <img
-          style={{width: 40, height: 40, marginRight: 12, verticalAlign: 'bottom' }}
-          src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-          alt="Ant Design"
-        />
-        Ant Design
-      </Title>
-    </section>
-    <Form labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <Form.Item label="数字输入框">
-        <InputNumber min={1} max={10} defaultValue={3} />
-        <span className="ant-form-text"> 台机器</span>
-        <a href="https://ant.design">链接文字</a>
-      </Form.Item>
-      <Form.Item label="开关">
-        <Switch defaultChecked />
-      </Form.Item>
-      <Form.Item label="滑动输入条">
-        <Slider defaultValue={70} />
-      </Form.Item>
-      <Form.Item label="选择器">
-        <Select defaultValue="lucy" style={{ width: 192 }}>
-          <Option value="jack">jack</Option>
-          <Option value="lucy">lucy</Option>
-          <Option value="disabled" disabled>disabled</Option>
-          <Option value="yiminghe">yiminghe</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item label="日期选择框">
-        <DatePicker />
-      </Form.Item>
+        <Title level={2}>
+          <img
+            style={{
+              width: 40,
+              height: 40,
+              marginRight: 12,
+              verticalAlign: "bottom",
+            }}
+            src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+            alt="Ant Design"
+          />
+          Ant Design
+        </Title>
 
-      <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button style={{ marginLeft: 8 }}>
-          Cancel
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form>
+            <Form.Item label="数字输入框">
+              <InputNumber min={1} max={10} defaultValue={3} />
+              <span className="ant-form-text"> 台机器</span>
+              <a href="https://ant.design">链接文字</a>
+            </Form.Item>
+            <Form.Item label="开关">
+              <Switch defaultChecked />
+            </Form.Item>
+            <Form.Item label="滑动输入条">
+              <Slider defaultValue={70} />
+            </Form.Item>
+            <Form.Item label="滑动输入条">
+              <Checkbox defaultChecked />
+              <Checkbox />
+              <Checkbox disabled defaultChecked />
+              <Checkbox disabled />
+            </Form.Item>
+            <Form.Item>
+            <Radio.Group>
+              <Radio value={1} defaultChecked>A</Radio>
+              <Radio value={2} >B</Radio>
+              <Radio value={3} defaultChecked disabled>C</Radio>
+              <Radio value={4} disabled>D</Radio>
+            </Radio.Group>
+            </Form.Item>
+            <Form.Item label="选择器">
+              <Select defaultValue="lucy" style={{ width: 192 }}>
+                <Option value="jack">jack</Option>
+                <Option value="lucy">lucy</Option>
+                <Option value="disabled" disabled>
+                  disabled
+                </Option>
+                <Option value="yiminghe">yiminghe</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="日期选择框">
+              <DatePicker />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <Button style={{ marginLeft: 8 }}>Cancel</Button>
+            </Form.Item>
+          </Form>
+          <Alert
+            className=""
+            message="Success Text"
+            description="Success Description Success Description Success Description"
+            type="success"
+          />
+          <Alert
+            message="Info Text"
+            description="Info Description Info Description Info Description Info Description"
+            type="info"
+          />
+          <Alert
+            message="Warning Text"
+            description="Warning Description Warning Description Warning Description Warning Description"
+            type="warning"
+          />
+          <Alert
+            message="Error Text"
+            description="Error Description Error Description Error Description Error Description"
+            type="error"
+          />
+          <Alert message="Success Tips" type="success" showIcon />
+          <Alert message="Informational Notes" type="info" showIcon />
+          <Alert message="Warning" type="warning" showIcon closable />
+          <Alert message="Error" type="error" showIcon />
+          <Alert
+            message="Success Tips"
+            description="Detailed description and advice about successful copywriting."
+            type="success"
+            showIcon
+          />
+          <Alert
+            message="Informational Notes"
+            description="Additional description and information about copywriting."
+            type="info"
+            showIcon
+          />
+          <Alert
+            message="Warning"
+            description="This is a warning notice about copywriting."
+            type="warning"
+            showIcon
+            closable
+          />
+          <Alert
+            message="Error"
+            description="This is an error message about copywriting."
+            type="error"
+            showIcon
+          />
   </>
 );
 
